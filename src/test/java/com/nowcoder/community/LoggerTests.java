2a980db4298d3741ed8dc9cecd12d4931c369474
han-hao-han <1787918931@qq.com>
2022-08-18 17:52:10 +0800

    add //test

--- src/test/java/com/nowcoder/community/LoggerTests.java
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class LoggerTests {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
-
+    //test
     @Test
     public void testLogger() {
         System.out.println(logger.getName());
